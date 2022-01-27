878a4ebd7a4b5fd935f7b1a4f3f38f8fd6c81d5f
Sayana <sayana_0308@mail.ru>
2022-01-27 14:57:50 +0800

    add phpdev-init

---
+++ phpdev-init.sh
@@ -0,0 +1,13 @@
+# Задание 6
+# Установка nginx, php, mysql, phpmyadmin
+
+sudo apt install nginx php-fpm mysql-server php-gd php-mysql phpmyadmin
+
+# Установка VSCode
+
+sudo dpkg -i code_1.63.2-1639562499_amd64.deb
+
+# Установка oh my zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
